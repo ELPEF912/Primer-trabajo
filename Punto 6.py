6a26b6f7e594209250390123025754caedb12d76
ELPEF912 <papumore2016@gmail.com>
2026-04-28 14:19:54 -0300

    Punto 6.py

--- Punto 6.py
+++ Punto 6.py
@@ -3,12 +3,15 @@
 
 binario = input("Ingresá un número binario: ") #Uso string porque necesito recorrer cada dígito del número binario.
 
-bits = [int(i) for i in binario] #Convierte todos los carácteres a números.
+if not all(i in "01" for i in binario): #Valida si alguien pone algo distinto de ceros y unos.
+    print("Ingresá solo 0 y 1.")
+else:
+    bits = [int(i) for i in binario] #Convierte todos los carácteres a números.
 
-decimal = 0
+    decimal = 0
 
-for i in range(len(bits)):
-    exponente = len(bits) - 1 - i
-    decimal += bits[i] * (2 ** exponente)
+    for i in range(len(bits)):
+        exponente = len(bits) - 1 - i
+        decimal += bits[i] * (2 ** exponente)
 
-print(f"El número decimal es: {decimal}")
+    print(f"El número decimal es: {decimal}")
